7423604586684136f8c8775eb2c4ebbfbaa79442
tfliss <t.fliss@gmail.com>
2016-01-12 01:26:53 -0800

    change bg map, add edited csv w/ example difficulties

--- folium_test.py
+++ folium_test.py
@@ -10,6 +10,11 @@ EASY = '#00FF00'
 INTERMEDIATE = '#FFFF00'
 DIFFICULT = '#FF0000'
 
+difficulties = { 'u' : UNCLASSIFIED,
+                 'e' : EASY,
+                 'i' : INTERMEDIATE,
+                 'd' : DIFFICULT }
+
 def read_kml(filename):
     f = open(filename, 'r')
     return f.read()
@@ -24,7 +29,8 @@ def process_kml(kml):
     segment_records = { 'name' : [],
                         'polyline': [],
                         'district' : [],
-                        'difficulty' : []
+                        'difficulty' : [],
+                        'difficulty_code' : []
                     }
     
     for layer in layers:
@@ -39,6 +45,7 @@ def process_kml(kml):
                 segment_records['polyline'].append(polyline)
                 segment_records['district'].append(layer.name)
                 segment_records['difficulty'].append(UNCLASSIFIED)
+                segment_records['difficulty_code'].append('u')
             except:
                 print "no geometry"
                 
@@ -59,7 +66,7 @@ def kml_to_map(coords, kml_file):
 
     k.from_string(kml_string)
 
-    map_osm = folium.Map(location=coords)    
+    map_osm = folium.Map(location=coords,tiles='Stamen Toner')    
     
     segment_df = process_kml(k)
     segment_df.to_csv('greenways.csv')
@@ -67,9 +74,22 @@ def kml_to_map(coords, kml_file):
     draw_lines(map_osm, segment_df)
 
     map_osm.create_map(path='osm.html')
+    
+    return segment_df
+
+def csv_to_map(coords, segment_df, csv_file, output_html):
+    edited_segment_df = pd.read_csv(csv_file, "\t")
+    edited_segment_df['difficulty'] = edited_segment_df['difficulty_code'].map(lambda d: difficulties[d.lower()])
+    edited_segment_df[['polyline']] = segment_df[['polyline']]
+    
+    map_osm = folium.Map(location=coords, tiles='Stamen Toner')
+    draw_lines(map_osm, edited_segment_df)
+    
+    map_osm.create_map(path=output_html)
 
 if __name__ == '__main__':
     coords = [47.6131746,-122.4878834] # Seattle
     # coords = [45.5236, -122.6750] # Portland
 
-    kml_to_map(coords, 'doc.kml')
+    segment_df = kml_to_map(coords, 'doc.kml')
+    csv_to_map(coords, segment_df, 'greenways_edited.csv', 'osm2.html')
